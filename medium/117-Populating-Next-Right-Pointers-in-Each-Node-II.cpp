e8c5d0738a23d819e66afec2185531495d856b63
ardxwe <ardxwe@gmail.com>
2021-09-21 09:31:22 -0400

    117-Populating-Next-Right-Pointers-in-Each-Node-II

---
+++ medium/117-Populating-Next-Right-Pointers-in-Each-Node-II.cpp
@@ -0,0 +1,65 @@
+/* Given a binary tree
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * Populate each next pointer to point to its next right node. If there is no
+ * next right node, the next pointer should be set to NULL. Initially, all next
+ * pointers are set to NULL.
+ */
+
+#include <vector>
+
+using std::vector;
+
+// Definition for a Node.
+class Node {
+ public:
+  int val;
+  Node* left;
+  Node* right;
+  Node* next;
+
+  Node() : val(0), left(nullptr), right(nullptr), next(nullptr) {}
+
+  explicit Node(int _val)
+      : val(_val), left(nullptr), right(nullptr), next(nullptr) {}
+
+  Node(int _val, Node* _left, Node* _right, Node* _next)
+      : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+class Solution {
+ public:
+  Node* connect(Node* root) {
+    if (root == nullptr) {
+      return nullptr;
+    }
+
+    Node* res = root;
+    vector<Node*> v;
+    v.push_back(root);
+    vector<Node*> temp;
+
+    while (!v.empty()) {
+      for (int i = 0; i < v.size() - 1; i++) {
+        v[i]->next = v[i + 1];
+      }
+      v[v.size() - 1]->next = nullptr;
+
+      for (auto pointer : v) {
+        if (pointer->left != nullptr) {
+          temp.push_back(pointer->left);
+        }
+        if (pointer->right != nullptr) {
+          temp.push_back(pointer->right);
+        }
+      }
+      v = temp;
+      temp.clear();
+    }
+    return res;
+  }
+};
